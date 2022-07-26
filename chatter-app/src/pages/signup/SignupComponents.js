5a4aab499de44620dda99b9ca4ba4858f3dcb879
nathanlauder <laudern42@gmail.com>
2022-07-25 23:00:22 -0500

    copy login components for styling, change names

---
+++ chatter-app/src/pages/signup/SignupComponents.js
@@ -0,0 +1,24 @@
+import {
+  LoginWrapper,
+  LoginContainer,
+  LoginTitle,
+  LoginLabel,
+  LoginInput,
+  SignupLinkContainer
+} from '../login/LoginComponents';
+
+const SignupWrapper = LoginWrapper;
+const SignupContainer = LoginContainer;
+const SignupTitle = LoginTitle;
+const SignupLabel = LoginLabel;
+const SignupInput = LoginInput;
+const LoginLinkContainer = SignupLinkContainer;
+
+export {
+  SignupWrapper,
+  SignupContainer,
+  SignupTitle,
+  SignupLabel,
+  SignupInput,
+  LoginLinkContainer
+};
