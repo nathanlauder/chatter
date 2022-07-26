cee161d00d47b8b3c5fd14b414b0050818b091f2
nathanlauder <laudern42@gmail.com>
2022-07-25 22:58:18 -0500

    create button link for signing up/in

---
+++ chatter-app/src/components/ButtonLink.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const ButtonLink = styled.button`
+  background-color: var(--frost) !important;
+  border: none;
+  border-radius: 5px;
+  padding: 0.5rem;
+  font-weight: 700;
+
+  
+  color: var(--white);
+  background-color: transparent;
+  text-decoration: none;
+  
+`;
+
+export default ButtonLink;
