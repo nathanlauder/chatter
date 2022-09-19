5c1ad479fb5629eba8298259fc4262f919afffba
nathanlauder <laudern42@gmail.com>
2022-09-18 19:19:43 -0500

    route message endpoints

--- api/router.js
+++ api/router.js
@@ -1,9 +1,11 @@
 const userRoutes = require('./routes/userRoutes');
 const conversationRoutes = require('./routes/conversationRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 
 const routeAllRoutes = (app) => {
   app.use('/api/v1/user', userRoutes);
   app.use('/api/v1/conversation', conversationRoutes);
+  app.use('/api/v1/message', messageRoutes);
 };
 
 module.exports = { routeAllRoutes };
