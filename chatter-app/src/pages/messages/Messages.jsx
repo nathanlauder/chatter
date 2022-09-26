8e4e0a128abf51ac4232b48ce2696c3b9288ad41
nathanlauder <laudern42@gmail.com>
2022-09-25 20:49:58 -0500

    fetch messages for each conversation

--- chatter-app/src/pages/messages/Messages.jsx
+++ chatter-app/src/pages/messages/Messages.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-// import Search from './components/search/Search';
 import redirect from '../../util/redirect';
 import Conversations from './components/conversations/Conversations';
 import MessageCards from './MessageCards';
 import MessageBar from './MessageBar';
 import { ls } from '../../Api/storage';
+
+import { post } from '../../Api/Api';
+import { getMessages as messageEndpoint } from '../../Api/Endpoints';
 
 const Container = styled.div`
   display: flex;
@@ -14,74 +16,21 @@ const Container = styled.div`
 
 const InnerContainer = styled.div`
   width: calc(100% - 300px);
+  min-width: calc(100% - 300px);
 `;
 
-const messageList = [
-  {
-    _id: '1',
-    sender: '62e9cf6f0fc404c6f22afd67',
-    text: 'hello',
-    timeStamp: new Date(Date.now())
-  },
-  {
-    _id: '2',
-    sender: '62e888133db6a3a9411e60c3',
-    text: 'hey',
-    timeStamp: new Date(Date.now())
-  },
-  {
-    _id: '3',
-    sender: '62e9cf6f0fc404c6f22afd67',
-    text: 'what\'s up',
-    timeStamp: new Date(Date.now())
-  },
-  {
-    _id: '4',
-    sender: '62e888133db6a3a9411e60c3',
-    text: 'not much bro',
-    timeStamp: new Date(Date.now())
-  },
-  {
-    _id: '5',
-    sender: '62e9cf6f0fc404c6f22afd67',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeStamp: new Date(Date.now())
-  },
-  {
-    _id: '6',
-    sender: '62e888133db6a3a9411e60c3',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeStamp: new Date(Date.now())
-  },
-  {
-    _id: '7',
-    sender: '62e9cf6f0fc404c6f22afd67',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeStamp: new Date(Date.now())
-  },
-  {
-    _id: '8',
-    sender: '62e888133db6a3a9411e60c3',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeStamp: new Date(Date.now())
-  },
-  {
-    _id: '9',
-    sender: '62e9cf6f0fc404c6f22afd67',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeStamp: new Date(Date.now())
-  },
-  {
-    _id: '10',
-    sender: '62e888133db6a3a9411e60c3',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    timeStamp: new Date(Date.now())
-  }
-];
+const UnselectedConversation = styled.div`
+  top: 50%;
+  left: calc(50% - 265px);
+  position: absolute;
+  font-size: 1.2rem;
+  background-color: transparent;
+`;
 
 const Messages = () => {
   const [isLoggedIn, setLoginStatus] = useState(false);
   const [activeConversation, setActiveConversation] = useState('');
+  const [messageList, setMessages] = useState([]);
 
   useEffect(() => {
     const loginStatusOptions = {
@@ -96,6 +45,15 @@ const Messages = () => {
     }
   }, []);
 
+  useEffect(() => {
+    const getMessages = async () => {
+      const payload = { conversationId: activeConversation };
+      const data = await post(messageEndpoint, payload);
+      setMessages(data);
+    };
+    getMessages();
+  }, [activeConversation]);
+
   const handleConversationClick = (conversationId) => {
     setActiveConversation(conversationId);
   };
@@ -105,9 +63,16 @@ const Messages = () => {
   ) : (
     <Container>
       <InnerContainer>
-        <MessageCards messageList={messageList} />
-        <MessageBar activeConversation={activeConversation} />
+        {activeConversation === '' ? (
+          <UnselectedConversation>Select a conversation</UnselectedConversation>
+        ) : (
+          <>
+            <MessageCards messageList={messageList} />
+            <MessageBar activeConversation={activeConversation} />
+          </>
+        )}
       </InnerContainer>
+
       <Conversations setActiveConversation={handleConversationClick} />
     </Container>
   );
