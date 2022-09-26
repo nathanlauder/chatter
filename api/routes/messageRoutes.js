c69091080640f0aefbb5e35d99eee13795762f72
nathanlauder <laudern42@gmail.com>
2022-09-25 20:46:13 -0500

    route for retrieve

--- api/routes/messageRoutes.js
+++ api/routes/messageRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { sendMessage } = require('../handlers/messages');
+const { sendMessage, getMessages } = require('../handlers/messages');
 
 router.post('/send', sendMessage);
+router.post('/retrieve', getMessages);
 
 module.exports = router;
