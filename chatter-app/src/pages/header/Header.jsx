13b84e906a50775cb861a81458ebe1156a38ec7a
Nathan Lauder <laudern42@gmail.com>
2022-10-29 16:27:22 -0500

    add pseudo logo

--- chatter-app/src/pages/header/Header.jsx
+++ chatter-app/src/pages/header/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
@@ -5,10 +6,20 @@ import PropTypes from 'prop-types';
 import { ProfilePicture, Title } from './HeaderComponents';
 
 const Container = styled.div`
-    height: 75px;
-    border-bottom: 2px solid var(--grey);
-    position: relative;
-  `;
+  height: 75px;
+  border-bottom: 2px solid var(--grey);
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.h1`
+  @import url('https://fonts.googleapis.com/css2?family=Satisfy&display=swap');
+  margin-left: 1rem;
+  font-family: 'Satisfy';
+  font-size: 2.75rem;
+`;
 
 const Header = ({ activeConversation }) => {
   // eslint-disable-next-line no-unused-vars
@@ -16,7 +27,8 @@ const Header = ({ activeConversation }) => {
 
   return (
     <Container>
-      <Title>{activeConversation}</Title>
+      {/* <Title>{activeConversation}</Title> */}
+      <Logo>Chatter</Logo>
       <ProfilePicture />
     </Container>
   );
