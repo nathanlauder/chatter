0dd2be27579e772a877224417b6e91991543967a
Nathan Lauder <laudern42@gmail.com>
2022-12-10 20:02:08 -0600

    WIP: sockets

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const socket = require('socket.io');
 
 const cors = require('cors');
 const { routeAllRoutes } = require('./router');
@@ -24,8 +25,51 @@ const connect = async () => {
 connect();
 routeAllRoutes(app);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+// eslint-disable-next-line no-unused-vars
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: `http://localhost:3000`, // frontends port
+    credentials: true,
+    methods: ['GET', 'POST'],
+    transports: ['websocket', 'polling']
+  },
+  allowEIO3: true
+});
+
+const usersOnline = new Map();
+const activeChats = new Map();
+
+io.on('connection', (sock) => {
+  global.chatSocket = sock;
+
+  sock.on('connect-user', (userId) => {
+    console.log(`Connection request recieved from user: ${userId}`);
+    usersOnline.set(userId, sock.id);
+  });
+
+  sock.on('connect-chat', (chatId) => {
+    console.log(`Connecting ${chatId} chat to sockets`);
+    // if (!activeChats.get(chatId))
+    activeChats.set(chatId, sock.id);
+    console.log(`ID of socket for connected chat: ${activeChats.get(chatId)}`);
+    return activeChats.get(chatId);
+  });
+
+  sock.on('send-msg', (data) => {
+    console.log(data);
+    const {
+      conversation, sender, text, timeStamp
+    } = data;
+    console.log(`Message "${text}" being sent to ${conversation} from ${sender} at ${timeStamp}`);
+    // need to figure out how to do for group convo
+    const activeChatSocket = activeChats.get(conversation);
+    console.log(`Active chat: ${activeChatSocket}`);
+    if (activeChatSocket) sock.to(activeChatSocket).emit('receive-msg', data);
+  });
 });
 
 module.exports = app;
